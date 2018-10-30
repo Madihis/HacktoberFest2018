0b842e9ab050b4933f6b408e04c14db29c29ab73
İhsan Alp <44585035+Madihis@users.noreply.github.com>
2018-10-30 21:01:52 +0300

    GörevTamam.md

--- Java/CodeKAL.java
+++ Java/CodeKAL.java
@@ -2,11 +2,8 @@ public class CodeKal {
 
 	public static void main(String[] args) {
 
-		// Aşağıdaki cümleleri ingilizceye çeviriniz.(Noktalama işaretlerinin Java için 
-		// özel anlamları var. Çevirirken silmeyiniz.)
-
-		System.out.println("I love CodeKAL.");
-		System.out.println("I'm happy that I'm in CodeKAL.");
+		System.out.println("CodeKal'ı seviyorum.");
+		System.out.println("CodeKal'da olduğum için mutluyum.");
 
 	}
 
